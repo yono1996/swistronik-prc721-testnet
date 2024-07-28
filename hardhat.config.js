a1490367ad67b43afac3fc75ecd2c3075d497507
yono1996 <39361116+yono1996@users.noreply.github.com>
2024-07-28 15:19:14 +0000

    feat: initiated the project

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`${process.env.PRIVATE_KEY}`],
+    },
+  },
+};
